1eae3d6a1e89cbe6abea5a49685134faea1f8848
mardefronteira <mmv341@nyu.edu>
2021-04-06 22:27:13 -0300

    Estilo e aviso para dispositivos mobile

--- nav.js
+++ nav.js
@@ -1,66 +1,26 @@
+let mobile = false;
+
 function iniciar() {
+  if (
+    navigator.userAgent.match(/Android/i) ||
+    navigator.userAgent.match(/webOS/i) ||
+    navigator.userAgent.match(/iPhone/i) ||
+    navigator.userAgent.match(/iPad/i) ||
+    navigator.userAgent.match(/iPod/i) ||
+    navigator.userAgent.match(/BlackBerry/i) ||
+    navigator.userAgent.match(/Windows Phone/i)
+  ) {
+    mobile = true;
+  }
+  console.log(navigator.userAgent);
+
   const botaoPlay = document.querySelector("#play-inicial");
-  const botaoSair = document.querySelector("#sair-menu");
-  const playPause = document.querySelector("#play-pause");
-  const botaoComecar = document.querySelector("#comecar-inicial");
+  const creditos = document.querySelector("#creditos");
   const botaoCreditos = Array.from(
     document.querySelectorAll(".botao-creditos")
   );
-  const botaoYoutube = document.querySelector("#youtube-inicial");
-  const botaoInsta = document.querySelector("#instagram-inicial");
-  const botaoSpotify = document.querySelector("#spotify-inicial");
-  const simbolosInicial = Array.from(
-    document.querySelectorAll(".simbolo-inicial")
-  );
-  const simbolosMenu = Array.from(document.querySelectorAll(".simbolo-menu"));
 
-  const tempoSimbolos = {
-    delay1: 0,
-    onda: 37,
-    lama: 60,
-    feedback: 87,
-    preto: 121,
-    lotus: 137,
-    simetria: 162,
-    delay2: 205,
-  };
-
-  for (let simbolo of simbolosInicial) {
-    const tempo = tempoSimbolos[simbolo.id.split("-")[0]];
-    simbolo.addEventListener("click", () => {
-      musica.currentTime = tempo;
-      clipeTodo = false;
-      mostrarPopup();
-    });
-  }
-
-  for (let simbolo of simbolosMenu) {
-    const tempo = tempoSimbolos[simbolo.id.split("-")[0]];
-    simbolo.addEventListener("click", () => {
-      musica.currentTime = tempo;
-      ampSine = 0.01;
-      musica.pause();
-      document.querySelector("#play-menu").classList.remove("escondida");
-      document.querySelector("#pause-menu").classList.add("escondida");
-    });
-  }
-
-  botaoPlay.addEventListener("click", () => {
-    musica.currentTime = 0;
-    clipeTodo = true;
-    mostrarPopup();
-  });
-
-  botaoSair.addEventListener("click", fecharClipe);
-
-  botaoComecar.addEventListener("click", () => {
-    pedirCamera();
-    checarCamera();
-    document.querySelector("#popup").classList.add("escondida");
-    document.querySelector("#creditos").classList.add("escondida");
-  });
-
-  const creditos = document.querySelector("#creditos");
+  // funcionamento dos créditos para todas as telas
   for (let botao of botaoCreditos) {
     botao.addEventListener("mouseover", () => {
       creditos.classList.remove("escondida");
@@ -76,20 +36,93 @@ function iniciar() {
         : creditos.classList.remove("escondida");
       creditoVisivel = !creditoVisivel;
     });
-  }
 
-  playPause.addEventListener("click", () => {
-    if (musica.paused) {
-      musica.play();
-      document.querySelector("#play-menu").classList.add("escondida");
-      document.querySelector("#pause-menu").classList.remove("escondida");
+    if (!mobile) {
+      // botões para o desktop
+      const playPause = document.querySelector("#play-pause");
+      const botaoSair = document.querySelector("#sair-menu");
+      const botaoComecar = document.querySelector("#comecar-inicial");
+      const botaoYoutube = document.querySelector("#youtube-inicial");
+      const botaoInsta = document.querySelector("#instagram-inicial");
+      const botaoSpotify = document.querySelector("#spotify-inicial");
+      const simbolosInicial = Array.from(
+        document.querySelectorAll(".simbolo-inicial")
+      );
+      const simbolosMenu = Array.from(
+        document.querySelectorAll(".simbolo-menu")
+      );
+
+      const tempoSimbolos = {
+        delay1: 0,
+        onda: 37,
+        lama: 60,
+        feedback: 87,
+        preto: 121,
+        lotus: 137,
+        simetria: 162,
+        delay2: 205,
+      };
+
+      for (let simbolo of simbolosInicial) {
+        const tempo = tempoSimbolos[simbolo.id.split("-")[0]];
+        simbolo.addEventListener("click", () => {
+          musica.currentTime = tempo;
+          clipeTodo = false;
+          mostrarPopup();
+        });
+      }
+
+      for (let simbolo of simbolosMenu) {
+        const tempo = tempoSimbolos[simbolo.id.split("-")[0]];
+        simbolo.addEventListener("click", () => {
+          musica.currentTime = tempo;
+          ampSine = 0.01;
+          musica.pause();
+          document.querySelector("#play-menu").classList.remove("escondida");
+          document.querySelector("#pause-menu").classList.add("escondida");
+        });
+      }
+
+      botaoPlay.addEventListener("click", () => {
+        musica.currentTime = 0;
+        clipeTodo = true;
+        mostrarPopup();
+      });
+
+      botaoSair.addEventListener("click", fecharClipe);
+
+      botaoComecar.addEventListener("click", () => {
+        pedirCamera();
+        checarCamera();
+        document.querySelector("#popup").classList.add("escondida");
+        document.querySelector("#creditos").classList.add("escondida");
+      });
+
+      playPause.addEventListener("click", () => {
+        if (musica.paused) {
+          musica.play();
+          document.querySelector("#play-menu").classList.add("escondida");
+          document.querySelector("#pause-menu").classList.remove("escondida");
+        } else {
+          musica.pause();
+          document.querySelector("#play-menu").classList.remove("escondida");
+          document.querySelector("#pause-menu").classList.add("escondida");
+          // deuPlay = false;
+        }
+      });
     } else {
-      musica.pause();
-      document.querySelector("#play-menu").classList.remove("escondida");
-      document.querySelector("#pause-menu").classList.add("escondida");
-      // deuPlay = false;
+      // botões para mobile
+      const popup = document.querySelector("#popup-mobile");
+      botaoPlay.addEventListener("click", () => {
+        popup.classList.remove("escondida");
+        creditos.classList.add("escondida");
+      });
+      const botaoOk = document.querySelector("#ok-inicial");
+      botaoOk.addEventListener(`click`, () => {
+        popup.classList.add("escondida");
+      });
     }
-  });
+  }
 }
 
 let creditoVisivel = false;
--- sketch.js
+++ sketch.js
@@ -40,51 +40,57 @@ let deuPlay = false;
 let ampSine = 0.01;
 
 function preload() {
-  // load the shader
-  videoFeedback = loadShader(
-    "shaders/shaders.vert",
-    "shaders/feedback.frag",
-    carregando
-  );
-  frameDiff = loadShader(
-    "shaders/shaders.vert",
-    "shaders/frameDiff.frag",
-    carregando
-  );
-  mosaic = loadShader(
-    "shaders/shaders.vert",
-    "shaders/mosaic.frag",
-    carregando
-  );
-  displacement = loadShader(
-    "shaders/shaders.vert",
-    "shaders/displacement.frag",
-    carregando
-  );
-  delay = loadShader("shaders/shaders.vert", "shaders/delay.frag", carregando);
-  sine = loadShader("shaders/shaders.vert", "shaders/sine.frag", carregando);
-  simetria = loadShader(
-    "shaders/shaders.vert",
-    "shaders/simetria.frag",
-    carregando
-  );
-  delayFinal = loadShader(
-    "shaders/shaders.vert",
-    "shaders/delayFinal.frag",
-    carregando
-  );
+  if (!mobile) {
+    // load the shader
+    videoFeedback = loadShader(
+      "shaders/shaders.vert",
+      "shaders/feedback.frag",
+      carregando
+    );
+    frameDiff = loadShader(
+      "shaders/shaders.vert",
+      "shaders/frameDiff.frag",
+      carregando
+    );
+    mosaic = loadShader(
+      "shaders/shaders.vert",
+      "shaders/mosaic.frag",
+      carregando
+    );
+    displacement = loadShader(
+      "shaders/shaders.vert",
+      "shaders/displacement.frag",
+      carregando
+    );
+    delay = loadShader(
+      "shaders/shaders.vert",
+      "shaders/delay.frag",
+      carregando
+    );
+    sine = loadShader("shaders/shaders.vert", "shaders/sine.frag", carregando);
+    simetria = loadShader(
+      "shaders/shaders.vert",
+      "shaders/simetria.frag",
+      carregando
+    );
+    delayFinal = loadShader(
+      "shaders/shaders.vert",
+      "shaders/delayFinal.frag",
+      carregando
+    );
 
-  shaders = [
-    delay,
-    displacement,
-    videoFeedback,
-    mosaic,
-    frameDiff,
-    sine,
-    simetria,
-    delayFinal,
-  ];
-  medulaOne = loadFont("fontes/MedulaOne-Regular.ttf");
+    shaders = [
+      delay,
+      displacement,
+      videoFeedback,
+      mosaic,
+      frameDiff,
+      sine,
+      simetria,
+      delayFinal,
+    ];
+    medulaOne = loadFont("fontes/MedulaOne-Regular.ttf");
+  }
 }
 
 function carregando() {
@@ -95,47 +101,49 @@ function carregando() {
 }
 let telaClipe;
 function setup() {
-  const canvasSize = {
-    w: windowWidth / 2,
-  };
+  if (!mobile) {
+    const canvasSize = {
+      w: windowWidth / 2,
+    };
 
-  telaClipe = createCanvas(windowWidth, windowHeight);
-  telaClipe.hide();
-  noStroke();
+    telaClipe = createCanvas(windowWidth, windowHeight);
+    telaClipe.hide();
+    noStroke();
 
-  musica = document.createElement("AUDIO");
-  musica.src = "midia/musica.wav";
-  musica.id = "musica";
-  musica.addEventListener("timeupdate", (e) => {
-    tempoMusica = musica.currentTime;
-  });
-  musica.addEventListener("ended", () => {
-    fecharClipe();
-    document.querySelector("#creditos").classList.remove("escondida");
-    creditoVisivel = true;
-  });
+    musica = document.createElement("AUDIO");
+    musica.src = "midia/musica.wav";
+    musica.id = "musica";
+    musica.addEventListener("timeupdate", (e) => {
+      tempoMusica = musica.currentTime;
+    });
+    musica.addEventListener("ended", () => {
+      fecharClipe();
+      document.querySelector("#creditos").classList.remove("escondida");
+      creditoVisivel = true;
+    });
 
-  document.body.appendChild(musica);
-  musica.load();
+    document.body.appendChild(musica);
+    musica.load();
 
-  videoFundo = createVideo("midia/fundo.mp4");
-  videoFundo.size(width, height);
-  videoFundo.hide();
+    videoFundo = createVideo("midia/fundo.mp4");
+    videoFundo.size(width, height);
+    videoFundo.hide();
 
-  camadaShader = createGraphics(windowWidth / 2, windowHeight / 2, WEBGL);
-  camadaCopia = createGraphics(windowWidth, windowHeight);
+    camadaShader = createGraphics(windowWidth / 2, windowHeight / 2, WEBGL);
+    camadaCopia = createGraphics(windowWidth, windowHeight);
 
-  // let proxima = createButton("⠀");
-  // proxima.position(20, 20);
-  // proxima.mousePressed(iniciar);
+    // let proxima = createButton("⠀");
+    // proxima.position(20, 20);
+    // proxima.mousePressed(iniciar);
 
-  // graphics para o efeito de delay
-  for (let i = 0; i < numLayers; i++) {
-    let l = createGraphics(windowWidth, windowHeight);
-    layers.push(l);
+    // graphics para o efeito de delay
+    for (let i = 0; i < numLayers; i++) {
+      let l = createGraphics(windowWidth, windowHeight);
+      layers.push(l);
+    }
+
+    angleMode(DEGREES);
   }
-
-  angleMode(DEGREES);
 }
 
 function pedirCamera() {
@@ -204,161 +212,163 @@ function escolherShader() {
 }
 
 function draw() {
-  if (shadersCarregadas) {
-    if (!deuPlay) {
-      background(0);
-      noFill();
-      stroke(255);
-      ellipse(width / 2, height / 2, mouseX, mouseY);
-    } else {
-      escolherShader();
+  if (!mobile) {
+    if (shadersCarregadas) {
+      if (!deuPlay) {
+        background(0);
+        noFill();
+        stroke(255);
+        ellipse(width / 2, height / 2, mouseX, mouseY);
+      } else {
+        escolherShader();
 
-      // shader() sets the active shader with our shader
-      camadaShader.shader(shaders[shaderAtiva]);
+        // shader() sets the active shader with our shader
+        camadaShader.shader(shaders[shaderAtiva]);
 
-      if (shaders[shaderAtiva] === mosaic) {
-        shaders[shaderAtiva].setUniform("tex0", camadaCopia);
-      } else if (![delay, delayFinal].includes(shaders[shaderAtiva])) {
-        shaders[shaderAtiva].setUniform("tex0", cam);
-      }
-
-      shaders[shaderAtiva] === displacement
-        ? videoFundo.loop()
-        : videoFundo.pause();
-
-      switch (shaders[shaderAtiva]) {
-        case videoFeedback:
-          // send the copy layer to the shader as a uniform
-          shaders[shaderAtiva].setUniform("tex1", camadaCopia);
-
-          shaders[shaderAtiva].setUniform("time", frameCount * 0.01);
-
-          // draw the camadaShader into the copy layer
-          camadaCopia.image(camadaShader, 0, 0, width, height);
-
-          break;
-
-        case frameDiff:
-          // enviar frame anterior à camada de cópia
-          shaders[shaderAtiva].setUniform("tex1", camadaCopia);
-
-          // also send the mouseX value but convert it to a number between 0 and 1
-          shaders[shaderAtiva].setUniform("mouseX", mouseX / width);
-
-          // rect gives us some geometry on the screen
-          camadaShader.rect(0, 0, width, height);
-
-          // draw the cam into the createGraphics layer at the very end of the draw loop
-          // because this happens at the end, if we use it earlier in the loop it will still be referencing an older frame
-          camadaCopia.image(cam, 0, 0, width, height);
-          break;
-
-        case simetria:
-          // enviar frame anterior à camada de cópia
-          shaders[shaderAtiva].setUniform("tex1", camadaCopia);
-
-          // also send the mouseX value but convert it to a number between 0 and 1
-          shaders[shaderAtiva].setUniform("mouseX", mouseX / width);
-
-          // rect gives us some geometry on the screen
-          camadaShader.rect(0, 0, width, height);
-
-          // draw the cam into the createGraphics layer at the very end of the draw loop
-          // because this happens at the end, if we use it earlier in the loop it will still be referencing an older frame
-          camadaCopia.image(cam, 0, 0, width, height);
-          break;
-
-        case mosaic:
+        if (shaders[shaderAtiva] === mosaic) {
           shaders[shaderAtiva].setUniform("tex0", camadaCopia);
+        } else if (![delay, delayFinal].includes(shaders[shaderAtiva])) {
+          shaders[shaderAtiva].setUniform("tex0", cam);
+        }
 
-          // send the resolution to the shader
-          shaders[shaderAtiva].setUniform("resolution", [width, height]);
+        shaders[shaderAtiva] === displacement
+          ? videoFundo.loop()
+          : videoFundo.pause();
 
-          lotus();
+        switch (shaders[shaderAtiva]) {
+          case videoFeedback:
+            // send the copy layer to the shader as a uniform
+            shaders[shaderAtiva].setUniform("tex1", camadaCopia);
 
-          // mandar tempo da música para shader
-          // shaders[shaderAtiva].setUniform("time", 20.0);
-          break;
+            shaders[shaderAtiva].setUniform("time", frameCount * 0.01);
 
-        case displacement:
-          // lets just send the cam to our shader as a uniform
-          shaders[shaderAtiva].setUniform("tex1", videoFundo);
+            // draw the camadaShader into the copy layer
+            camadaCopia.image(camadaShader, 0, 0, width, height);
 
-          shaders[shaderAtiva].setUniform(
-            "amt",
-            map(tempoMusica, 60, 85.5, -1.0, 1.0)
-          );
-          break;
+            break;
 
-        case delay:
-          // draw the camera on the current layer
-          layers[index1].image(cam, 0, 0, width, height);
+          case frameDiff:
+            // enviar frame anterior à camada de cópia
+            shaders[shaderAtiva].setUniform("tex1", camadaCopia);
 
-          // send the camera and the two other past frames into the camera feed
-          shaders[shaderAtiva].setUniform("tex0", layers[index1]);
-          shaders[shaderAtiva].setUniform("tex1", layers[index2]);
-          shaders[shaderAtiva].setUniform("tex2", layers[index3]);
+            // also send the mouseX value but convert it to a number between 0 and 1
+            shaders[shaderAtiva].setUniform("mouseX", mouseX / width);
 
-          // increase all indices by 1, resetting if it goes over layers.length
-          // the index runs in a circle 0, 1, 2, ... 29, 30, 0, 1, 2, etc.
-          // index1
-          // index2 will be somewhere in the past
-          // index3 will be even further into the past
-          index1 = (index1 + 1) % layers.length;
-          index2 = (index2 + 1) % layers.length;
-          index3 = (index3 + 1) % layers.length;
-          break;
+            // rect gives us some geometry on the screen
+            camadaShader.rect(0, 0, width, height);
 
-        case delayFinal:
-          // draw the camera on the current layer
-          layers[index1].image(cam, 0, 0, width, height);
+            // draw the cam into the createGraphics layer at the very end of the draw loop
+            // because this happens at the end, if we use it earlier in the loop it will still be referencing an older frame
+            camadaCopia.image(cam, 0, 0, width, height);
+            break;
 
-          // send the camera and the two other past frames into the camera feed
-          shaders[shaderAtiva].setUniform("tex0", layers[index1]);
-          shaders[shaderAtiva].setUniform("tex1", layers[index2]);
-          shaders[shaderAtiva].setUniform("tex2", layers[index3]);
+          case simetria:
+            // enviar frame anterior à camada de cópia
+            shaders[shaderAtiva].setUniform("tex1", camadaCopia);
 
-          // increase all indices by 1, resetting if it goes over layers.length
-          // the index runs in a circle 0, 1, 2, ... 29, 30, 0, 1, 2, etc.
-          // index1
-          // index2 will be somewhere in the past
-          // index3 will be even further into the past
-          index1 = (index1 + 1) % layers.length;
-          index2 = (index2 + 1) % layers.length;
-          index3 = (index3 + 1) % layers.length;
-          break;
+            // also send the mouseX value but convert it to a number between 0 and 1
+            shaders[shaderAtiva].setUniform("mouseX", mouseX / width);
 
-        case sine:
-          shaders[shaderAtiva].setUniform("time", frameCount * 0.01);
+            // rect gives us some geometry on the screen
+            camadaShader.rect(0, 0, width, height);
 
-          if (tempoMusica < 56) {
-            ampSine += 0.0001;
-          } else {
-            ampSine -= 0.017;
-          }
+            // draw the cam into the createGraphics layer at the very end of the draw loop
+            // because this happens at the end, if we use it earlier in the loop it will still be referencing an older frame
+            camadaCopia.image(cam, 0, 0, width, height);
+            break;
 
-          let amp = map(mouseY, height, 0, 0, 0.1) + ampSine;
+          case mosaic:
+            shaders[shaderAtiva].setUniform("tex0", camadaCopia);
 
-          shaders[shaderAtiva].setUniform("amplitude", amp);
-          break;
+            // send the resolution to the shader
+            shaders[shaderAtiva].setUniform("resolution", [width, height]);
 
-        default:
+            lotus();
+
+            // mandar tempo da música para shader
+            // shaders[shaderAtiva].setUniform("time", 20.0);
+            break;
+
+          case displacement:
+            // lets just send the cam to our shader as a uniform
+            shaders[shaderAtiva].setUniform("tex1", videoFundo);
+
+            shaders[shaderAtiva].setUniform(
+              "amt",
+              map(tempoMusica, 60, 85.5, -1.0, 1.0)
+            );
+            break;
+
+          case delay:
+            // draw the camera on the current layer
+            layers[index1].image(cam, 0, 0, width, height);
+
+            // send the camera and the two other past frames into the camera feed
+            shaders[shaderAtiva].setUniform("tex0", layers[index1]);
+            shaders[shaderAtiva].setUniform("tex1", layers[index2]);
+            shaders[shaderAtiva].setUniform("tex2", layers[index3]);
+
+            // increase all indices by 1, resetting if it goes over layers.length
+            // the index runs in a circle 0, 1, 2, ... 29, 30, 0, 1, 2, etc.
+            // index1
+            // index2 will be somewhere in the past
+            // index3 will be even further into the past
+            index1 = (index1 + 1) % layers.length;
+            index2 = (index2 + 1) % layers.length;
+            index3 = (index3 + 1) % layers.length;
+            break;
+
+          case delayFinal:
+            // draw the camera on the current layer
+            layers[index1].image(cam, 0, 0, width, height);
+
+            // send the camera and the two other past frames into the camera feed
+            shaders[shaderAtiva].setUniform("tex0", layers[index1]);
+            shaders[shaderAtiva].setUniform("tex1", layers[index2]);
+            shaders[shaderAtiva].setUniform("tex2", layers[index3]);
+
+            // increase all indices by 1, resetting if it goes over layers.length
+            // the index runs in a circle 0, 1, 2, ... 29, 30, 0, 1, 2, etc.
+            // index1
+            // index2 will be somewhere in the past
+            // index3 will be even further into the past
+            index1 = (index1 + 1) % layers.length;
+            index2 = (index2 + 1) % layers.length;
+            index3 = (index3 + 1) % layers.length;
+            break;
+
+          case sine:
+            shaders[shaderAtiva].setUniform("time", frameCount * 0.01);
+
+            if (tempoMusica < 56) {
+              ampSine += 0.0001;
+            } else {
+              ampSine -= 0.017;
+            }
+
+            let amp = map(mouseY, height, 0, 0, 0.1) + ampSine;
+
+            shaders[shaderAtiva].setUniform("amplitude", amp);
+            break;
+
+          default:
+        }
+
+        // criar geometria ao fim, exceto para frameDiff, que precisa que seja antes
+        [frameDiff, simetria].includes(shaders[shaderAtiva])
+          ? ""
+          : camadaShader.rect(0, 0, width, height);
+
+        // draw the cam into the copy layer
+        if (shaders[shaderAtiva] === simetria) {
+          camadaCopia.image(camadaShader, 0, 0, width, height);
+        } else if (shaders[shaderAtiva] !== mosaic) {
+          camadaCopia.image(cam, 0, 0, width, height);
+        }
+
+        // render the camadaShader to the screen
+        image(camadaShader, 0, 0, width, height);
       }
-
-      // criar geometria ao fim, exceto para frameDiff, que precisa que seja antes
-      [frameDiff, simetria].includes(shaders[shaderAtiva])
-        ? ""
-        : camadaShader.rect(0, 0, width, height);
-
-      // draw the cam into the copy layer
-      if (shaders[shaderAtiva] === simetria) {
-        camadaCopia.image(camadaShader, 0, 0, width, height);
-      } else if (shaders[shaderAtiva] !== mosaic) {
-        camadaCopia.image(cam, 0, 0, width, height);
-      }
-
-      // render the camadaShader to the screen
-      image(camadaShader, 0, 0, width, height);
     }
   }
 }
